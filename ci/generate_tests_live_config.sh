25a440dc1d122f769fb77c21def6b8b7226f5415
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2018-01-23 21:40:23 -0500

    Remove todo (done in previous commit)

--- ci/generate_tests_live_config.sh
+++ ci/generate_tests_live_config.sh
@@ -11,10 +11,3 @@ echo "      \"user_id\": ${USER_ID},"                     >> tests_live_config.j
 echo "      \"start_date\": \"${START_DATE}\","           >> tests_live_config.json
 echo "      \"end_date\": \"${END_DATE}\""                >> tests_live_config.json
 echo "}"                                                  >> tests_live_config.json
-
-# TODO: create an account at toggl for actual testing and run these commands
-# gem install travis
-# travis encrypt --add WORKSPACE_NAME=workspace-name
-# travis encrypt --add TOKEN=xxxx
-# travis encrypt --add WORKSPACE_ID=123456
-# travis encrypt --add USER_ID=123456
